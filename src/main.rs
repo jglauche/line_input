e83ff2b7b1794561279289f0c63394a7c38f0073
Jennifer Glauche <mail@jglauche.de>
2019-11-01 19:45:47 +0100

    making a line input thing with a buffer that can be deleted

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::io::Write;
+use std::error::Error;
+use console::{Key, Term};
+
+fn line_input(query: &'static str, default: &'static str) -> Result<(String), Box<dyn Error>> {
+	let mut buf = String::from(default);
+	let mut term = Term::stdout();
+	let mut clear = true;
+
+	loop{
+		if clear == true {
+			term.clear_line()?;
+			term.write(&query.to_string().as_bytes())?;
+			term.write(b" ")?;
+			term.write(&buf.to_string().as_bytes())?;
+			clear = false;
+		}
+		match term.read_key()? {
+			Key::Char(c) => {
+				buf = buf + &c.to_string();
+				term.write(&c.to_string().as_bytes())?;
+			},
+			Key::Backspace => {
+				if buf.len() > 0 {
+					buf.truncate(buf.len()-1);
+					clear = true;
+				}
+			},
+			Key::Enter => {
+				term.write_line("")?;
+				break
+			},
+			_ => {}
+		}
+	}
+	Ok(buf)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+	let res = line_input("What is your name?", "Cat").expect("not to fail");
+	println!("Got input: {}", res);
+	Ok(())
+}
